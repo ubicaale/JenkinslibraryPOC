acf2c69ee95917abc1e7d3ea49dfa92a3f61b365
ubicaale <37723130+ubicaale@users.noreply.github.com>
2023-12-02 18:06:57 +0800

    Create pipeline1.groovy

---
+++ pipeline1.groovy
@@ -0,0 +1,16 @@
+stage ('Clone work'){
+    parallel (
+        'Stage B1': {
+              sh 'echo Initial Part Finished'
+           },
+         'Stage B2':{
+            stage ('Stage A_1'){
+              sh 'echo Ping Machine A'
+            }
+         },
+        'Stage B3': {
+          stage ("Stage A3"){
+            sh ' echo Collect Info'
+          }
+        }
+    )}
